754cb58bc670d4e6ff1f8181006254f57d035a20
Simon Cornelius <simonjoecornelius@gmail.com>
2025-04-15 20:41:39 +0200

    feat: add daa

--- src/instruction/accumulator.rs
+++ src/instruction/accumulator.rs
@@ -9,3 +9,33 @@ pub fn cpl(cpu: &mut cpu::CPU) {
 
     cpu.pc += 1;
 }
+
+pub fn daa(cpu: &mut cpu::CPU) {
+    let mut adjustment = 0;
+    let mut a = cpu.get_register(util::Register::A);
+    if cpu.flags.subtraction {
+        if cpu.flags.half_carry {
+            adjustment += 0x6;
+            if cpu.flags.carry {
+                adjustment += 0x60;
+            }
+        }
+        a = a.wrapping_sub(adjustment);
+    } else {
+        if cpu.flags.half_carry || a & 0xF > 9 {
+            adjustment += 0x6;
+        }
+        if cpu.flags.carry || a > 0x99 {
+            cpu.flags.carry = true;
+            adjustment += 0x60;
+        }
+        a = a + adjustment;
+    }
+
+    cpu.set_register(util::Register::A, a);
+
+    cpu.flags.zero = a == 0;
+    cpu.flags.half_carry = false;
+
+    cpu.pc += 1;
+}
