6c6bf0588e306c42785656a72927c8c5cb2e9207
Simon Cornelius <simonjoecornelius@gmail.com>
2025-12-25 12:19:42 +0100

    delete ui frontend
    first aim should be to get the emulator running and have the ui attach
    to the emulator and not the other way around

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,12 @@
-use crate::emulator::{Emulator, EmulatorMessage, Handle, State};
-use eframe::egui;
-use emulator::disassemble::disassemble;
-use std::sync::mpsc::TryRecvError;
-use std::sync::TryLockError;
+use crate::emulator::{Emulator, Handle};
 use std::{env, fs};
-use ui::{breakpoints::BreakpointView, disasm::DisassemblyView, settings::SettingsView};
 
 pub mod emulator;
 
-mod ui;
-
-const COLORS: catppuccin::FlavorColors = catppuccin::PALETTE.macchiato.colors;
-
-pub struct UiState {
-    emulator_state: State,
-    last_pc: u16,
-    bottom_panel_selected_tab: usize,
-    settings_view: SettingsView,
-    disassembly_view: DisassemblyView,
-    breakpoint_view: BreakpointView,
-}
-
-impl Default for UiState {
-    fn default() -> Self {
-        Self {
-            emulator_state: State::Paused,
-            last_pc: 0,
-            bottom_panel_selected_tab: 0,
-            settings_view: SettingsView::default(),
-            disassembly_view: DisassemblyView::default(),
-            breakpoint_view: BreakpointView::default(),
-        }
-    }
-}
+// this struct should basically represent the ui
 
 struct BamegoyApp {
     emulator_handle: Handle,
-    ui_state: UiState,
 }
 
 impl BamegoyApp {
@@ -56,13 +26,15 @@ impl BamegoyApp {
 
         Self {
             emulator_handle: handle,
-            ui_state: UiState::default(),
         }
     }
 }
 
-fn main() -> eframe::Result {
+fn main() {
     env_logger::init();
+
+    // handling cli args is neither the main's responsiblity, nor the emulators
+    // so like any other user input, it should be handled by the ui
     let mut args: Vec<String> = env::args().collect();
 
     let rom_filepath = args.pop();
@@ -74,56 +46,5 @@ fn main() -> eframe::Result {
         }
     }
 
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([680.0, 720.0]),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "bamegoy",
-        options,
-        Box::new(|cc| {
-            egui_extras::install_image_loaders(&cc.egui_ctx);
-
-            Ok(Box::new(BamegoyApp::new(rom_filepath, should_trace_log)))
-        }),
-    )
-}
-
-impl eframe::App for BamegoyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        match self.emulator_handle.rx.try_recv() {
-            Ok(msg) => {
-                match msg {
-                    EmulatorMessage::Paused => {
-                        self.ui_state.emulator_state = State::Paused;
-                    },
-                    EmulatorMessage::Running => {
-                        self.ui_state.emulator_state = State::Running;
-                    },
-                }
-            },
-            Err(e) => {
-                if e != TryRecvError::Empty {
-                    ui::draw_error(ctx, "Channel Disconnected".to_string());
-                    return;
-                }
-            },
-        }
-
-        if self.ui_state.emulator_state == State::Running {
-            ui::draw_running(ctx, &mut self.emulator_handle);
-        } else {
-            match (self.emulator_handle.cpu.try_lock(), self.emulator_handle.bus.try_lock()) {
-                (Ok(mut cpu), Ok(mut bus)) => {
-                    ui::draw(ctx, &mut self.ui_state, self.emulator_handle.tx.clone(), &mut *bus, &mut *cpu);
-                }
-                (Err(TryLockError::WouldBlock), _) => ui::draw_error(ctx, "acquiring cpu handle...".to_string()),
-                (_, Err(TryLockError::WouldBlock)) => ui::draw_error(ctx, "acquiring bus handle...".to_string()),
-                (Err(TryLockError::Poisoned(_)), _) |
-                (_, Err(TryLockError::Poisoned(_))) => {
-                    ui::draw_error(ctx, "CPU or Bus lock poisoned!".to_string());
-                }
-            }
-        }
-    }
+    BamegoyApp::new(rom_filepath, should_trace_log)
 }
